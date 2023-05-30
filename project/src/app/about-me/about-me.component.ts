bfb5f06ab830712a6dac4b778c79c59052a14d01
Everson-Menezes <everson.menezes@fatec.sp.gov.br>
2023-05-30 13:47:01 -0300

    Feature - About-me: Development

--- project/src/app/about-me/about-me.component.ts
+++ project/src/app/about-me/about-me.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
-export class AboutMeComponent { }
+export class AboutMeComponent implements OnInit {
+
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/img/angular.jpg',
+    };
+    this.slides[1] = {
+      src: './assets/img/react.jpg',
+    }
+    this.slides[2] = {
+      src: './assets/img/vue.jpg',
+    }
+  }
+}
